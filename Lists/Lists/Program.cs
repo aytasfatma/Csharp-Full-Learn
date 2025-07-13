92cd9dee80496de77c4338733b561525bca83de9
fatma aytaş <fatmaytis34@gmail.com>
2025-07-13 16:38:33 +0300

    updated

---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+// dizi ile 
+string[] sehirler = new string[] { "ankara", "istanbul", "izmir" };
+Console.WriteLine(sehirler.Length);
+sehirler = new string[4];
+sehirler[3] = "bursa";
+foreach (var sehir in sehirler)
+{
+
+    Console.WriteLine(sehir);
+
+}
+//liste ile 
+List<String> sehiler = new List<string>()
+ { "ankara","istanbul","izmir"};
+Console.WriteLine(sehiler.Count);
+sehiler.Add("bursa");
+sehiler.Remove("istanbul");
+sehiler.Add("artvin");
+bool sonuc = sehiler.Contains("izmir");
+Console.WriteLine(sehiler.Count);
+Console.WriteLine(sonuc);
